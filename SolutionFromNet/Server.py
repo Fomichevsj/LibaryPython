ffe0f34d414352fa781e29a67a5210bc68210636
Fomichev Sergey <fomichevsj@gmail.com>
2017-12-26 00:45:09 +0300

    работает доступ к серверу через несколько клиентов
    пока что нереализована работа с книгами

--- SolutionFromNet/Server.py
+++ SolutionFromNet/Server.py
@@ -4,9 +4,12 @@ import sys
 
 import time
 
+import select
+
 sock = socket.socket()
 sock.bind(('', 1080))
 sock.listen(3)
+#sock.setblocking(0)
 conn = []
 data = ''
 def Reciver():
@@ -37,9 +40,39 @@ def Sender():
         if data:
             for i in range(len(conn)):
                 print('Отправляю клиенту: ', i)
-                ans = "Ответ от сервака".encode() + data
+                ans = "Ответ от сервака: ".encode() + data
                 conn[i].send(ans)
             data = 0
+
+def SenderAndRecier():
+    global data
+    global conn
+    while 1:
+        print('in reciever')
+        time.sleep(2)
+        for i in range(len(conn)):
+            print('слушаем клиента ', i)
+            try:
+                ready = select.select(list(conn), [], [], 4)
+                print('Попытаемся получить сообщение 2')
+                print(ready[0])
+                print(conn[i])
+                if ready[0]:
+                    data = ready[0][0].recv(1024)
+                    print('Клиент', i,  'активен. Получили сообщение.')
+                if data:
+                    print('Пришло сообщение!')
+                    print(data.decode())
+                    print('Отправляю клиенту: ', i)
+                    ans = "Ответ от сервака: ".encode() + data
+                    ready[0][0].send(ans)
+                    data = 0
+            except socket.error as e:
+                if e.errno == 10053:
+                    conn.pop(i)
+                    print("Подключено пользователй:", len(conn))
+                else:
+                    raise
 
 def Accepter():
     while 1:
@@ -51,11 +84,13 @@ def Accepter():
 
 
 # init threads
-t1 = threading.Thread(target=Reciver)
-t2 = threading.Thread(target=Sender)
+#t1 = threading.Thread(target=Reciver)
+#t2 = threading.Thread(target=Sender)
+t11 = threading.Thread(target=SenderAndRecier)
 t3 = threading.Thread(target=Accepter)
 
 # start threads
-t1.start()
-t2.start()
+#t1.start()
+#t2.start()
+t11.start()
 t3.start()
